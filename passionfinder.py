a3688b4c1ff5379a1843dc3336ed84eab7616f85
Erika-create <65840745+Erika-create@users.noreply.github.com>
2026-07-20 10:21:31 +0000

    basic questions

--- passionfinder.py
+++ passionfinder.py
@@ -1,1 +1,12 @@
-print("Hello, world!")
+
+#main
+subList = ["Maths", "Physics", "Biology", "Chemistry", "Computer Science"]
+jobList = ["アプリ開発", "AI研究", "ウェブデザイン", "ビジネス・企画"]
+print("これはCSに興味のある女子高生のための大学での科目選択シミュレーターです。")
+
+n1 = input("現在の文理への興味を教えて（1=理系、2=文系、3=わからない）：")
+print(n1)
+n2 = input("数学への苦手意識はどれくらい（1=大好き、2=好き、3=普通、4=少し苦手、5=苦手）：")
+print(n2)
+print("将来やってみたいことは？", jobList)
+#n3 = input("将来やってみたいことは？")
