e3ff84b7dd9d402d73380e3026cf99293df8139f
Erika-create <65840745+Erika-create@users.noreply.github.com>
2026-07-20 12:22:19 +0000

    verification completed

--- passionfinder.py
+++ passionfinder.py
@@ -5,7 +5,9 @@ score_core_cs = 0
 score_data_sci = 0
 score_creative = 0
 
-s1 = int(input("現在の文理への興味を教えて（1=理系、2=文系、3=わからない）："))
+s1 = 0
+while s1 != 1 and s1 != 2 and s1 != 3:
+    s1 = int(input("現在の文理への興味を教えて（1=理系、2=文系、3=わからない）："))
 if s1 == 1:
     score_core_cs += 3
 elif s1 == 3:
@@ -15,7 +17,9 @@ else:
     score_data_sci += 2
     score_creative += 2
 
-s2 = input("数学への意欲（A=好き、B=普通、C=苦手）：")
+s2 = ""
+while s2 != "A" and s2 != "B" and s2 != "C":
+    s2 = input("数学への意欲（A=好き、B=普通、C=苦手）：")
 if s2 == "A":
     score_core_cs += 4
     score_data_sci += 3
@@ -23,20 +27,25 @@ elif s2 == "B":
     score_data_sci += 2
     score_creative += 2
 else:
-    score_core_cs -= 5 #CS should not be selected if not good at maths
+    score_core_cs -= 5
     score_creative += 4
 
-s3 = input("数IIIは取ろうと思う・または取っている？（y/n）：")
+s3 = ""
+while s3 != "y" and s3 != "n":
+    s3 = input("数IIIは取ろうと思う・または取っている？（y/n）：")
 if s3 == "y":
     score_core_cs += 5
     score_data_sci += 2
 else:
-    score_core_cs -= 5 #if no mathsIII, not much chance in core cs
+    score_core_cs -= 5
 
-print("将来やってみたいことは？")
-for i in range(len(jobList)):
-    print("・", i+1, "=", jobList[i])
-s4 = int(input())
+
+s4 = 0
+while s4 < 1 or s4 > len(jobList)+1:
+    print("将来やってみたいことは？")
+    for i in range(len(jobList)):
+        print("・", i+1, "=", jobList[i])
+    s4 = int(input())
 if s4 == 1 or s4 == 2 or s4 == 3:
     score_core_cs += 3
     score_creative += 2
@@ -45,24 +54,30 @@ elif s4 == 4 or s4 == 5:
 else:
     score_data_sci += 5
 
-print("何かを作るとしたら、どっちにワクワクする？")
-s5 = int(input("1=パズルのような謎解きや、仕組み・ロジックを考えること、2=イラストを描く、動画を作る、見た目をデザインすること"))
+s5 = 0
+while s5 != 1 and s5 != 2:
+    print("何かを作るとしたら、どっちにワクワクする？")
+    s5 = int(input("1=パズルのような謎解きや、仕組み・ロジックを考えること、2=イラストを描く、動画を作る、見た目をデザインすること"))
 if s5 == 1:
     score_core_cs += 4
     score_data_sci += 3
 else:
     score_creative += 4
 
-print("\nスマホのアプリやゲームがバグったり、日常のルールで不便なことがあったとき、どう思う？")
-s6 = int(input("1=「なんでこうなるんだろう？」と原因を突き止めたり、裏側の仕組みを調べたくなる。\n2=「もっとこうなれば使いやすいのに！」と、新しいアイデアや改善策を考える方が楽しい。"))
+s6 = 0
+while s6 != 1 and s6 != 2:
+    print("\nスマホのアプリやゲームがバグったり、日常のルールで不便なことがあったとき、どう思う？")
+    s6 = int(input("1=「なんでこうなるんだろう？」と原因を突き止めたり、裏側の仕組みを調べたくなる。\n2=「もっとこうなれば使いやすいのに！」と、新しいアイデアや改善策を考える方が楽しい。"))
 if s6 == 1:
     score_core_cs += 3
 else:
     score_data_sci += 3
     score_creative += 3
 
-print("\n学校の探究学習や文化祭の準備、趣味の作業で、一番時間を忘れて熱中できる瞬間はどれ？")
-s7 = int(input("1=1人で黙々と作業して、パズルを解くように形を完成させていくとき（個人作業）。\n2=友達とアイデアを出し合ったり、全体の計画を立てて進め方をまとめるとき（チーム）。"))
+s7 = 0
+while s7 != 1 and s7 != 2:
+    print("\n学校の探究学習や文化祭の準備、趣味の作業で、一番時間を忘れて熱中できる瞬間はどれ？")
+    s7 = int(input("1=1人で黙々と作業して、パズルを解くように形を完成させていくとき（個人作業）。\n2=友達とアイデアを出し合ったり、全体の計画を立てて進め方をまとめるとき（チーム）。"))
 if s7 == 1:
     score_core_cs += 3
     score_data_sci += 2
